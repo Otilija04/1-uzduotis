cee8a56c9e5453571fdc2b773e9eb3a7dddf5163
Otilija04 <145570265+Otilija04@users.noreply.github.com>
2023-10-29 09:02:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "Library.h"
 #include <chrono>
 #include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
 
 bool fileExists(const std::string& fileName) {
-    std::ifstream file(fileName.c_str());
+    std::ifstream file(fileName);
     return file.good();
 }
 
@@ -15,22 +18,25 @@ void generateAndSaveStudents(int numStudents, const std::string& fileName, doubl
         generateStudents(numStudents, students);
         saveStudentsToFile(students, fileName);
         auto fileCreateEnd = std::chrono::high_resolution_clock::now();
-
         fileCreateDuration = std::chrono::duration<double>(fileCreateEnd - fileCreateStart).count();
         std::cout << "studentu kurimas uztruko " << fileCreateDuration << " sec." << std::endl;
     } else {
         std::cout << "File " << fileName << " already exists. Skipping generation." << std::endl;
-        fileCreateDuration = 0.0; 
+        fileCreateDuration = 0.0;
     }
 }
 
 void readAndProcessStudents(const std::string& fileName, double& fileReadDuration, double& fileSortDuration) {
     auto fileReadStart = std::chrono::high_resolution_clock::now();
-    std::vector<Student> students = readStudentData(fileName);
+    std::vector<Student> students;
+
+    students = readStudentData(fileName);
+    auto fileReadEnd = std::chrono::high_resolution_clock::now();
+    fileReadDuration = std::chrono::duration<double>(fileReadEnd - fileReadStart).count();
+    std::cout << "studentu nuskaitymas uztruko " << fileReadDuration << " sec." << std::endl;
 
     auto fileSortStart = std::chrono::high_resolution_clock::now();
-
-    for (Student& student : students) {
+    for (auto& student : students) {
         int sum = 0;
         for (int grade : student.homeworkGrades) {
             sum += grade;
@@ -40,32 +46,44 @@ void readAndProcessStudents(const std::string& fileName, double& fileReadDuratio
 
     std::vector<Student> badStudents, goodStudents;
     separateStudentsByAverage(students, badStudents, goodStudents);
-
     auto fileSortEnd = std::chrono::high_resolution_clock::now();
     fileSortDuration = std::chrono::duration<double>(fileSortEnd - fileSortStart).count();
-
     std::cout << "studentu rusiavimas i dvi grupes uztruko " << fileSortDuration << " sec." << std::endl;
-
-    auto fileReadEnd = std::chrono::high_resolution_clock::now();
-    fileReadDuration = std::chrono::duration<double>(fileReadEnd - fileReadStart).count();
-    std::cout << "studentu nuskaitymas uztruko " << fileReadDuration << " sec." << std::endl;
 
     saveStudentsToFile(badStudents, "bad" + fileName);
     saveStudentsToFile(goodStudents, "good" + fileName);
 }
 
 int main() {
-    int numStudents[] = {1000, 10000, 100000, 1000000, 10000000};
+    std::vector<int> numStudents = {1000, 10000, 100000, 1000000, 10000000};
+    int numTests = 5;
 
-    for (int i = 0; i < 5; ++i) {
-        std::string fileName = "kursiokai" + std::to_string(numStudents[i]) + ".txt";
-        std::cout << numStudents[i] << " studentu spartos analize:" << "\n";
+    for (int num : numStudents) {
+        std::string fileName = "kursiokai" + std::to_string(num) + ".txt";
+        std::cout << num << " studentu spartos analize:" << "\n";
 
-        double fileCreateDuration, fileReadDuration, fileSortDuration;
-        generateAndSaveStudents(numStudents[i], fileName, fileCreateDuration);
-        readAndProcessStudents(fileName, fileReadDuration, fileSortDuration);
+        double avgFileCreateDuration = 0.0;
+        double avgFileReadDuration = 0.0;
+        double avgFileSortDuration = 0.0;
 
-        std::cout << "studentu idejimas i du naujus failus uztruko " << fileCreateDuration + fileSortDuration + fileReadDuration << " sec." << std::endl;
+        for (int j = 0; j < numTests; ++j) {
+            double fileCreateDuration, fileReadDuration, fileSortDuration;
+            generateAndSaveStudents(num, fileName, fileCreateDuration);
+            readAndProcessStudents(fileName, fileReadDuration, fileSortDuration);
+
+            avgFileCreateDuration += fileCreateDuration;
+            avgFileReadDuration += fileReadDuration;
+            avgFileSortDuration += fileSortDuration;
+        }
+
+        avgFileCreateDuration /= numTests;
+        avgFileReadDuration /= numTests;
+        avgFileSortDuration /= numTests;
+
+        std::cout << "Vidutine failo kurimo trukme: " << avgFileCreateDuration << " sec." << std::endl;
+        std::cout << "Vidutine failo nuskaitymo trukme: " << avgFileReadDuration << " sec." << std::endl;
+        std::cout << "Vidutine rusiavimo trukme: " << avgFileSortDuration << " sec." << std::endl;
+
         std::cout << "\n\n";
     }
 
